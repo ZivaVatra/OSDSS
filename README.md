e523210525ec1d94a4aa7c7e1d1bf3552ee4077d
Ziva Vatra <zv@ziva-vatra.com>
2016-02-28 12:38:48 +0000

    Added version 1 of the tests

--- README.md
+++ README.md
@@ -7,12 +7,13 @@ So, back in 2012 I started work on an open source deep sky stacker. You can see 
 
 
 However dev stalled when I had to move to the city for work (the light pollution is so awful here that there is nothing to see at night but the moon and orange haze :-(  ).
-Rather than write my own image processing program, this is a wrapper script around other tools.
 
-Which other tools I will have to double check, which I will do once I put the code up here.
+## Goals
+At this point, I am just investigating which method is best for deep sky stacking, as a result I will add scripts and snippets to this repo as work progresses. I don't think I am even near an alpha quality piece of software, but you got to start somewhere :-)
 
-## Does it work
 
-Yep! No idea how it compares to commercial/freeware stackers, but for a first attempt it was pretty good IMO. Direct link below, or just go to the page linked above.
 
-http://www.ziva-vatra.com/image.php?render=67&name=U2lkZV9ieV9zaWRl
+Current versions:
+	- deepsky_stack_vers1.sh --  Bash script that (ab)uses panotools/enfuse/nona from the hugin project to align and fuse images together. 
+See http://www.ziva-vatra.com/image.php?render=67&name=U2lkZV9ieV9zaWRl for an example of 12 images stacked together using this script.
+
---
+++ deepsky_stack_vers1.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# vim: ts=4 autoindent 
+
+#===============================================================================
+#
+# Bash script that makes use of the hugin project panotools to stack
+# multiple images together, for noise reduction of deep sky shots. 
+#
+# Copyright 2016 Ziva-Vatra (http://www.ziva-vatra.com, mail: zv@ziva-vatra.com)
+#
+# Project website: http://www.ziva-vatra.com/index.php?aid=67&id=U29mdHdhcmU=
+# Project repo: https://github.com/ZivaVatra/OSDSS
+#
+#===============================================================================
+#
+# Licensed under the GNU GPL. Do not remove any information from this header
+# (or the header itself). If you have modified this code, feel free to add your
+# details below this (and by all means, mail me, I like to see what other people
+# have done)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License (version 2)
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+#===============================================================================
+
+
+if [[ $1 == "" ]]; then
+        echo "No input specified! Please give list of images to stack"
+	echo "Recommended to use tiff for processing..."
+        exit -1
+fi
+
+UUID=`head -c 8 /dev/urandom | md5sum | cut -d' ' -f 1`
+EXTDIR="./deepsky_"$UUID
+
+echo "Using $EXTDIR as output"
+if [ ! -d "$EXTDIR" ]; then
+    #if DIR does not exist, create
+    mkdir -v $EXTDIR
+fi
+
+
+#First, align images
+align_image_stack -i  -p Project_$UUID.pto $@  #-a astroshot_$UUID $@
+nona -v -m TIFF_m -o ./astroshot_$UUID  ./Project_$UUID.pto ./*.tif
+#move them to correct dir
+mv -v ./astroshot_$UUID* ./$EXTDIR/
+
+cd $EXTDIR
+#time enfuse-mp -v -l 29 -o deepsky_1.tif ./astroshot*.tif
+#time enfuse-mp -v -d 16 -l 29  -o deepsky_2.tif ./astroshot*.tif
+time enfuse-mp -v  -l 29  -o deepsky_1.tif ./astroshot*.tif
+time enfuse-mp --compression=LZW -v -d 16 -l 29 --entropy-cutoff=95% --entropy-weight=0.4 --gray-projector=luminance -o deepsky_2.tif ./astroshot*.tif
